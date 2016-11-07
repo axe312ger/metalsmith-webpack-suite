6e1123cb18ce5844bee75562350b197b667f34ed
Benedikt Rötsch <info@benedikt-roetsch.de>
2016-11-08 00:50:26 +0100

    proper webpack bundle injection

--- src/scripts/build-metalsmith.js
+++ src/scripts/build-metalsmith.js
@@ -4,8 +4,6 @@ import assets from 'metalsmith-assets'
 
 import paths from '../config/paths'
 
-const __DEV__ = process.env.NODE_ENV !== 'production'
-
 module.exports = new Metalsmith(paths.projectRoot)
   // .clean(false) reenable for cache plugin
   .source(paths.metalsmithSource)
@@ -18,21 +16,24 @@ module.exports = new Metalsmith(paths.projectRoot)
     destination: './assets'
   }))
   .use((files, metalsmith, done) => {
-    let html = files['index.html'].contents.toString()
     const assets = JSON.parse(files['assets/webpack-assets.json'].contents.toString())
 
-    if (__DEV__) {
-      // const assetsHead = `<script type="text/javascript">${assets['loader.js'].text}</script>`
-      const assetsHead = `<script src="assets/loader.js"></script>`
-
-      html = html.replace('<!-- assets-head -->', assetsHead)
-    } else {
-      const assetsHead = `<script src="${assets.head.js}"></script><link rel="stylesheet" type="text/css" href="${assets.styles.css}">`
-      const assetsBody = `<script src="${assets.page.js}"></script>`
-
-      html = html.replace('<!-- assets-head -->', assetsHead)
-      html = html.replace('<!-- assets-body -->', assetsBody)
+    const assetsHead = []
+    if (assets.hasOwnProperty('loader')) {
+      assetsHead.push(`<script src="${assets.loader.js}"></script>`)
+      delete assets.loader
     }
+
+    assetsHead.push(`<script src="${assets.head.js}"></script>`)
+    delete assets.head
+
+    const assetsBody = Object.keys(assets).map((asset) => {
+      return `<script src="${assets[asset].js}"></script>`
+    })
+
+    const html = files['index.html'].contents.toString()
+      .replace('<!-- assets-head -->', assetsHead.join('\n'))
+      .replace('<!-- assets-body -->', assetsBody.join('\n'))
 
     files['index.html'].contents = new Buffer(html)
 
