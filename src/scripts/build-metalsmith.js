c12fa42cc6918d926a09ffb2567a4a4130e6fbdb
Benedikt Rötsch <info@benedikt-roetsch.de>
2017-04-02 17:52:12 +0200

    refactor(dev-server): replace metalsmith-watch with browser-sync

--- src/scripts/build-metalsmith.js
+++ src/scripts/build-metalsmith.js
@@ -1,7 +1,10 @@
+// Builds Metalsmith based on the metalsmith.js.
+// Script used by the build commands for deploying the page.
+
 import Debug from 'debug'
 import metalsmith from './metalsmith'
 
-const debug = Debug('server-webpack')
+const debug = Debug('metalsmith-webpack-suite')
 
 metalsmith.build(function (err) {
   if (err) {
